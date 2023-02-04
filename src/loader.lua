b2904d236c06a592ccc4aaba8f1b6eb4f698c9b4
wait-what314 <83979811+wait-what314@users.noreply.github.com>
2023-02-04 17:37:35 +0300

    Update loader.lua

--- src/loader.lua
+++ src/loader.lua
@@ -185,13 +185,13 @@ local function CommandBoxFocusLost(EnterPressed)
     CommandBox.Text = ""
 end
 
-local function WorkspaceDescendantAdded(Descendant)
-    if Values.AntiFling and Descendant:IsA("Tool") then
-        local Handle = Descendant:WaitForChild("Handle", 3)
+local function WorkspaceAncestryChanged(Child, NewParent)
+    if Values.AntiFling and Child:IsA("Tool") then
+        local Handle = Child:WaitForChild("Handle", 3)
         if not Handle or not Handle:IsA("BasePart") then return end
 
         Handle.CanTouch = false
-        local TouchInterest = Descendant:WaitForChild("TouchInterest", 3)
+        local TouchInterest = Child:WaitForChild("TouchInterest", 3)
         if not TouchInterest then return end
 
         task.defer(TouchInterest.Destroy, TouchInterest)
@@ -205,7 +205,7 @@ table.insert(Connections, RunService.Stepped:Connect(OnStepped))
 table.insert(Connections, RunService.RenderStepped:Connect(OnRenderStepped))
 table.insert(Connections, CommandBox.Changed:Connect(CommandBoxChanged))
 table.insert(Connections, CommandBox.FocusLost:Connect(CommandBoxFocusLost))
-table.insert(Connections, workspace.DescendantAdded:Connect(WorkspaceDescendantAdded))
+table.insert(Connections, workspace.AncestryChanged:Connect(WorkspaceAncestryChanged))
 for _, Player in next, Players:GetPlayers() do
     task.spawn(OnPlayerAdded, Player)
 end
